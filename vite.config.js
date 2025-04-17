812ae8cb7cf8dc355fb668ab35d09025d9d4f171
Reinhard Bonke <rbonke17@gmail.com>
2025-04-17 19:48:03 +0300

    fiede gitconfig 2

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    host: true, // Allows access from the local network
-    port: 5173, //
+  server:{
+    host:true,
+    port:5173
+  },
+  plugins: [react(), tailwindcss()],
 })
