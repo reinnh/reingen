866bb53d08598150f77753817dbea165de433a30
Reinhard Bonke <rbonke17@gmail.com>
2025-04-17 19:36:08 +0300

    corrects the viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server:{
-    host:'0.0.0.0',
-    port:5173
-  }
+  server: {
+    host: true, // Allows access from the local network
+    port: 5173, //
 })
